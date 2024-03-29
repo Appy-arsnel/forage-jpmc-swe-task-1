5c3f5f8ff4fa62d9996ba4c62141c43642d8a67e
Appy-arsnel <75736454+Appy-arsnel@users.noreply.github.com>
2024-03-29 22:43:20 +0530

    commit

--- client3.py
+++ client3.py
@@ -42,6 +42,8 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Updated this function ------------- """
+    if price_b == 0:
+        return
     return price_a / price_b
 
 
